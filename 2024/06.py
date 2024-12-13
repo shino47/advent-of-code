4267c09c13a5bb1936b6ccec14000919f349ca04
Francisco Lau <francisco.lau@globalbank.com.pa>
2024-12-12 19:37:48 -0500

    wip

--- 2024/06.py
+++ 2024/06.py
@@ -2,7 +2,14 @@ class AoCY2024D06:
 
     def __init__(self):
         self.mapa = self.get_map()
-        self.x, self.y = self.get_start()
+        self.limit_y = len(self.mapa)
+        self.limit_x = len(self.mapa[0])
+        self.steps = {
+            'N': (0, -1, 'E'),
+            'E': (1, 0, 'S'),
+            'S': (0, 1, 'W'),
+            'W': (-1, 0, 'N'),
+        }
 
     def get_map(self):
         with open('inputs/06.txt') as file:
@@ -14,3 +21,33 @@ class AoCY2024D06:
                 if '^' == val:
                     return x, y
         return 0, 0
+
+    def get_next_pos(self, pos_x, pos_y, direction):
+        x = pos_x
+        y = pos_y
+        step_x, step_y, new_dir = self.steps[direction]
+        x += step_x
+        y += step_y
+        if x < 0 or y < 0 or x >= self.limit_x or y >= self.limit_y:
+            return False
+        if '#' == self.mapa[y][x]:
+            return self.get_next_pos(pos_x, pos_y, new_dir)
+        return x, y, direction
+
+    def get_part1(self):
+        direction = 'N'
+        x, y = self.get_start()
+        visited = {f"{x},{y}"}
+        while True:
+            val = self.get_next_pos(x, y, direction)
+            if val is False:
+                break
+            x = val[0]
+            y = val[1]
+            direction = val[2]
+            visited.add(f"{x},{y}")
+        return len(visited)
+
+
+aoc = AoCY2024D06()
+print('Answer 1:', aoc.get_part1())
