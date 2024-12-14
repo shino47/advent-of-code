f33909e16b1fb05a810aa01a18d53495971c84f5
Francisco Lau <francisco.lau@globalbank.com.pa>
2024-12-13 20:24:24 -0500

    wip

--- 2024/06.py
+++ 2024/06.py
@@ -4,6 +4,10 @@ class AoCY2024D06:
         self.mapa = self.get_map()
         self.limit_y = len(self.mapa)
         self.limit_x = len(self.mapa[0])
+        self.block_char = '#'
+        self.free_char = '.'
+        self.start_char = '^'
+        self.start_x, self.start_y = self.get_start()
         self.steps = {
             'N': (0, -1, 'E'),
             'E': (1, 0, 'S'),
@@ -13,12 +17,12 @@ class AoCY2024D06:
 
     def get_map(self):
         with open('inputs/06.txt') as file:
-            return tuple(tuple(line.strip()) for line in file)
+            return [list(line.strip()) for line in file]
 
     def get_start(self):
         for y, row in enumerate(self.mapa):
             for x, val in enumerate(row):
-                if '^' == val:
+                if self.start_char == val:
                     return x, y
         return 0, 0
 
@@ -30,13 +34,14 @@ class AoCY2024D06:
         y += step_y
         if x < 0 or y < 0 or x >= self.limit_x or y >= self.limit_y:
             return False
-        if '#' == self.mapa[y][x]:
+        if self.mapa[y][x] == self.block_char:
             return self.get_next_pos(pos_x, pos_y, new_dir)
         return x, y, direction
 
     def get_part1(self):
         direction = 'N'
-        x, y = self.get_start()
+        x = self.start_x
+        y = self.start_y
         visited = {f"{x},{y}"}
         while True:
             val = self.get_next_pos(x, y, direction)
@@ -48,6 +53,44 @@ class AoCY2024D06:
             visited.add(f"{x},{y}")
         return len(visited)
 
+    def test_path(self):
+        direction = 'N'
+        x = self.start_x
+        y = self.start_y
+        corners = ''
+        lasts = []
+        while True:
+            val = self.get_next_pos(x, y, direction)
+            if val is False:
+                break
+            x = val[0]
+            y = val[1]
+            pos = f"{x},{y}"
+            if direction != val[2]:
+                lasts.append(pos)
+                corners += f"|{pos}"
+                if len(lasts) > 3:
+                    changes = '|'.join(lasts)
+                    if changes in corners:
+                        print(changes, corners)
+                        return True
+                    lasts.pop(0)
+            direction = val[2]
+        return False
+
+    def get_part2(self):
+        paths = 0
+        for y, row in enumerate(self.mapa):
+            for x, char in enumerate(row):
+                if char != self.free_char:
+                    continue
+                self.mapa[y][x] = self.block_char
+                if self.test_path():
+                    paths += 1
+                self.mapa[y][x] = self.free_char
+        return paths
+
 
 aoc = AoCY2024D06()
 print('Answer 1:', aoc.get_part1())
+print('Answer 2:', aoc.get_part2())
