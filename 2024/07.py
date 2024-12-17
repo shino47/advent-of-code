bde484b9372e842e24f59a2b790caf5c4e6dd334
Francisco Lau <francisco.lau@globalbank.com.pa>
2024-12-16 19:20:58 -0500

    Day 7

--- 2024/07.py
+++ 2024/07.py
@@ -2,7 +2,7 @@ class AoCY2024D07:
 
     def __init__(self):
         self.input = self.get_input()
-        self.operations = ('+', '*')
+        self.operators = ('+', '*', '|')
 
     def get_input(self):
         with open('inputs/07.txt') as file:
@@ -13,14 +13,14 @@ class AoCY2024D07:
                 values.append(tuple([int(parts[0]), numbers]))
             return tuple(values)
 
-    def get_permutations(self, qty, current='', result=None):
+    def get_permutations(self, operators, qty, current='', result=None):
         if result is None:
             result = []
         if len(current) == qty:
             result.append(current)
             return result
-        for op in self.operations:
-            self.get_permutations(qty, current + op, result)
+        for op in operators:
+            self.get_permutations(operators, qty, current + op, result)
         return result
 
     def merge_operations(self, values, permutations):
@@ -47,19 +47,32 @@ class AoCY2024D07:
             if 0 == i:
                 value += item
                 continue
-            if item in self.operations:
+            if item in self.operators:
                 action = item
                 continue
             if '+' == action:
                 value += item
-            else:
+            elif '*' == action:
                 value *= item
+            elif '|' == action:
+                value = int(str(value) + str(item))
         return value
 
     def get_part1(self):
         total = 0
+        operators = ('+', '*')
         for target, values in self.input:
-            permutations = self.get_permutations(len(values) - 1)
+            permutations = self.get_permutations(operators, len(values) - 1)
+            operations = self.merge_operations(values, permutations)
+            if self.test_operations(target, operations):
+                total += target
+        return total
+
+    def get_part2(self):
+        total = 0
+        operators = ('+', '*', '|')
+        for target, values in self.input:
+            permutations = self.get_permutations(operators, len(values) - 1)
             operations = self.merge_operations(values, permutations)
             if self.test_operations(target, operations):
                 total += target
@@ -68,4 +81,4 @@ class AoCY2024D07:
 
 aoc = AoCY2024D07()
 print('Answer 1:', aoc.get_part1())
-# print('Answer 2:', aoc.get_part2())
+print('Answer 2:', aoc.get_part2())
